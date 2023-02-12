564d9981be02cbbd3ae08371b92e6f20236af19c
sfill70 <sergfill70@yandex.ru>
2023-02-12 22:33:42 +0300

    yml settings experiment

--- src/main/java/ru/javarush/country/redis/connection/Connection.java
+++ src/main/java/ru/javarush/country/redis/connection/Connection.java
@@ -3,6 +3,7 @@ package ru.javarush.country.redis.connection;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 
 import java.io.File;
 import java.io.IOException;
@@ -10,16 +11,26 @@ import java.io.IOException;
 public class Connection {
     private final ConnectionSetting connectionSetting;
     private final ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-    private final File file = new File(classLoader.getResource("settingsRedis.xml").getFile());
+    private final File xmlSetting = new File(classLoader.getResource("settingsRedis.xml").getFile());
+    private final File yamlSetting = new File(classLoader.getResource("settingsRedis.yaml").getFile());
 
     public Connection() {
+        ConnectionSetting connectionSettingLoading;
         ObjectMapper mapperXlm = new XmlMapper();
         mapperXlm.enable(SerializationFeature.INDENT_OUTPUT);
         try {
-            this.connectionSetting = mapperXlm.readValue(file, ConnectionSetting.class);
+            connectionSettingLoading = mapperXlm.readValue(xmlSetting, ConnectionSetting.class);
+//            int x = 1/0;
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            ObjectMapper mapperYaml = new YAMLMapper();
+            mapperYaml.enable(SerializationFeature.INDENT_OUTPUT);
+            try {
+                connectionSettingLoading = mapperYaml.readValue(yamlSetting, ConnectionSetting.class);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
         }
+        this.connectionSetting = connectionSettingLoading;
     }
 
     public String getConnectionHost() {
